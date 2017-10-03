6ef11e17126b9bf7daf40dd27f2915a941d11f6a
Joseph Surla <josephsurla@gmail.com>
2017-10-03 08:27:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+let dodger = document.getElementById('dodger')
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerLeft() {
+  let positionNumbers = dodger.style.left.replace('px', '')
+  let position = parseInt(positionNumbers, 10)
+
+  if (position > 0) {
+    dodger.style.left = `${position - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let positionNumbers = dodger.style.left.replace('px', '')
+  let position = parseInt(positionNumbers, 10)
+
+  if (position < 360) {
+    dodger.style.left = `${position + 1}px`
+  }
+}
