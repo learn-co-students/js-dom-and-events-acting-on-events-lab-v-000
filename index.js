6bf13efc1a1232c77bd65d93c7171c8075c8796c
peterckim <peterckim96@gmail.com>
2018-09-21 20:09:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+function preventRefreshOnSubmit() {
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  });
 }
 
-preventRefreshOnSubmit()
+function retrieveEmployeeInformation() {
+  return document.querySelector('input[name="name"]').value;
+}
+
+function addNewElementAsLi() {
+  let listElement = `${retrieveEmployeeInformation()}`;
+  document.querySelector("ul.employee-list").append(listElement);
+}
+
+function addNewLiOnClick() {
+  document
+    .querySelector('input[type="submit"]')
+    .addEventListener("click", function(event) {
+      addNewElementAsLi();
+      document.querySelector('input[name="name"]').value = "";
+    });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector("a");
+  let ul = document.querySelector("ul");
+  link.addEventListener("click", function(event) {
+    ul.innerHTML = "";
+  });
+}
+
+preventRefreshOnSubmit();
