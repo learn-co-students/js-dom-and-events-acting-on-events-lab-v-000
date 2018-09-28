7b8a97ef1ca9c9003691b56cabc65e60c3414b7c
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-28 22:53:52 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let target = document.querySelector('.employee-list');
+  let li = document.createElement("li");
+  li.innerText = retrieveEmployeeInformation();
+  target.appendChild(li);
+}
+
+ function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', addNewElementAsLi());
+  document.querySelector('input').value = '';
+ }
+
+ function clearEmployeeListOnLinkClick() {
+    let link = document.querySelector('a');
+    link.addEventListener('click', function(event) {
+      document.querySelector('.employee-list').innerHTML = '';
+    });
+ }
