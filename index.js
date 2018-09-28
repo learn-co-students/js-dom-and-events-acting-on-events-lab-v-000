28cac5884c4c22ca721d367d1c96ca2bc295aefc
NeedsAUsername <kevinchi118@gmail.com>
2018-09-27 22:07:59 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+  return document.querySelector("input[type='text']").value;
+}
+
+function addNewElementAsLi() {
+  let info = retrieveEmployeeInformation();
+  newLi = document.createElement('li');
+  newLi.textContent = info;
+  document.querySelector('.employee-list').appendChild(newLi);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector("input[type='submit']");
+  submit.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    document.querySelector("input[type='text']").value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let list = document.querySelector('.employee-list');
+  let clear = document.querySelector('a');
+  clear.addEventListener('click', function(event) {
+    list.textContent = "";
+  })
+}
