f39966456619579cef0281efb1c7767947d657c7
Katrina Brinson <kbrinso@gmail.com>
2018-10-01 02:39:17 +0000

    work in progress

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+    return document.querySelector('input[name="name"]').value;
+}
+const einput = document.querySelector('eform');
+let emp = einput.addEventListener('submit',retrieveEmployeeInformation);
+
+
+function addNewElementAsLi(){
+  let newemp = document.createElement('li').innerHTML = ;
+  return document.querySelector('ul.employee-list').appendChild(newemp);
+}
