297765550b7c90b89627834136c36c23f9603691
Katrina Brinson <kbrinso@gmail.com>
2018-10-01 05:01:46 +0000

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,31 @@ function retrieveEmployeeInformation(){
     return document.querySelector('input[name="name"]').value;
 }
 const einput = document.querySelector('eform');
-let emp = einput.addEventListener('submit',retrieveEmployeeInformation);
+const elist = $('.employee-list')
+einput.addEventListener('submit',retrieveEmployeeInformation);
+einput.addEventListener('submit',addNewElementAsLi);
+elist.addEventListener('click',addNewLiOnClick);
 
 
 function addNewElementAsLi(){
-  let newemp = document.createElement('li').innerHTML = ;
-  return document.querySelector('ul.employee-list').appendChild(newemp);
+  emp = retrieveEmployeeInformation;
+  let newemp = document.createElement('li').innerHTML = emp;
+  return $('ul.employee-list').append(newemp);
+}
+
+function addNewLiOnClick(){
+  emp = retrieveEmployeeInformation;
+  let newemp = document.createElement('li').innerHTML = emp;
+  $('ul.employee-list').append(newemp);
+  return document.querySelector('input[name="name"]').value = "";
+}
+
+
+
+function clearEmployeeListOnLinkClick(){
+  let btn = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  btn.addEventListener('click',function(){
+    ul.innerHTML = ''
+  });
 }
