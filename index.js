1ecd2a094109797d005fbe60d28d6c4cdb1418b9
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 19:17:21 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const retrieveEmployeeInformation = function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li')
+  document.querySelector('ul').appendChild(li);
+  li.append(`${retrieveEmployeeInformation()}`);
+  document.querySelector('input').value = '';
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', addNewElementAsLi);
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clear = document.querySelector('a');
+  clear.addEventListener('click', function() {
+    document.querySelector('ul').innerHTML = "";
+  })
+}
