8f75895fea01631b3985837a682758d6c4554bbf
Jul Jen <juliajen.prog@gmail.com>
2018-10-10 19:16:38 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+// retrieveEmployeeInformation(): This retrieves the employee information from the input. It returns a string equal to the value in the input.
+//
+// addNewElementAsLi(): This function takes the value retrieved from the previous retrieveEmployeeInformation function and adds that string to a new list element which is appended to the ul with class "employee-list".
+//
+// addNewLiOnClick(): This retrieves the employee information from the input.
+//
+// retrieveEmployeeInformation(): This retrieves the employee information from the input.
+//
+// retrieveEmployeeInformation(): This retrieves the employee information from the input.
