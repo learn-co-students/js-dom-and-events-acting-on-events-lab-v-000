b01bb00fa30331df13a171b2f43b03facb2381c3
Katrina Pritchard <pritckat@gmail.com>
2018-10-15 17:07:56 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
+let info = document.querySelector('input')
+
 function retrieveEmployeeInformation() {
-  return $('form')
+  return info.value;
+}
+
+function addNewElementAsLi() {
+  let name = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${name}</li>`)
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    info.value = ''
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let linkClick = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  linkClick.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
 }
