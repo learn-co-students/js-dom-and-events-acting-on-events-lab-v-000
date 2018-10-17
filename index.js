b607469d5e6e6a46a950f6853b890e4b77bb145a
github username <github email address>
2018-10-17 10:52:01 -0600

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,34 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
+let input = document.querySelector('input')
 
-function retrieveEmployeeInformation(event){
-  document.querySelector('form').addEventListener('submit', function(event){
-    console.log(event.target)
-  })
+function retrieveEmployeeInformation(){
+  console.log(input.value)
+  return input.value
 }
 
 function addNewElementAsLi(){
-  return $("ul.employee-list").append(retrieveEmployeeInformation())
+   $("ul.employee-list").append(retrieveEmployeeInformation())
+}
+
+function resetInput(){
+  input.value = ''
 }
 
 function addNewLiOnClick(){
+  let submit = document.querySelector('#submit_button')
+  submit.addEventListener('click', function(){
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let clear = document.querySelector('#clear')
+  clear.addEventListener('click', function(){
+    let ul = document.querySelector('ul')
+    ul.innerHTML = ''
+  })
 
 }
