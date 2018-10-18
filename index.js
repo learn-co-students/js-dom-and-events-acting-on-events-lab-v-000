e197030d44090478eae243548dcfb5ca63b93edb
Shirley Collins <shirley@m104.us>
2018-10-17 18:05:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+  })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  let txt = document.createTextNode(retrieveEmployeeInformation());
+  li.appendChild(txt);
+
+  return document.querySelector('ul.employee-list').append(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function() {
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clear = document.querySelector('a');
+
+  clear.addEventListener('click', function(event) {
+    document.querySelector('ul').innerHTML = ''
+  });
+}
