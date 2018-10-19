b16e365af35de2ca5f12fe7b381e70df231bb7a3
amckean12 <amckean@me.com>
2018-10-19 14:47:03 -0500

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
+let input = document.querySelector('input')
+
+const submit = document.querySelector('input[type="submit"]')
+
 function retrieveEmployeeInformation(){
-  
+  return input.value
+}
+
+function addNewElementAsLi(){
+  //Getting input from our text box
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick(){
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    document.querySelector('input').value = null
+  })
+}
+
+function clearEmployeeListOnLinkClick (){
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+    ul.innerHTML = null
+  })
 }
