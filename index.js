9586024c3c352a87a14f74774dd65fd51e030f17
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-25 12:46:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+    document.querySelector('form').addEventListener('submit', function(e){
+      e.preventDefault()
     })
 }
 
 preventRefreshOnSubmit()
+
+const input = document.querySelector('input')
+let submit = document.querySelector('.clear')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi(){
+ const li = document.createElement('li')
+ li.appendChild(document.createTextNode(input.value))
+}
+
+function addNewLiOnClick(){
+
+}
+
+function clearEmployeeListOnLinkClick(){
+  localStorage.clear()
+}
