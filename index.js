df652e4b2ab2bfe7f8c7d82e55a1db25c13bcbe0
zep1994 <snaxbags@protonmail.com>
2018-11-06 00:34:31 -0600

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let list = document.querySelector('.employee-list');
+  list.append(retrieveEmployeeInformation());
+}
+
+function addNewLiOnClick(){
+  let input = document.querySelector('input[type="submit"]');
+  input.addEventListener('click', function(e){
+    addNewElementAsLi()
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  link.addEventListener('click', function(e) {
+    document.querySelector('ul').innerHTML = '';
+  });
+}
