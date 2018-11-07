807972a142bd993405cf25431aa5c3988192b498
github username <github email address>
2018-11-07 13:20:17 -0500

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ function addNewElementAsLi() {
   let li = document.createElement('li');
   li.innerHTML = employeeName;
   document.querySelector(".employee-list").appendChild(li);
+  // Code above results in the name in a list and then a blank li item
+  // The solution code also results in a list item and then a blank li item
+  // document.querySelector('.employee-list').append(li).innerHTMl;
+  // Code in line 24 creates a name per list with no blank li items, but fails test.
+  // input field does not clear, and clear employee list does not work.
 };
 
 
