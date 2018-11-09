6b1b183d471a5084d68fdfa232e2c8244753381d
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-11-09 12:03:28 -0800

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let name = retrieveEmployeeInformation()
+
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${name}</li>`)
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+  document.querySelector('input').value = ''
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = ''
+  })
+}
