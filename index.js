2c343b317c6dc827baccb045cf5811a49156dff9
ElleeB <laura.trager@gmail.com>
2018-11-09 15:24:18 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input[type="text"]').value
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li')
+  let ul = document.querySelector("ul.employee-list")
+  let list = ul.appendChild(li)
+  list.innerHTML = retrieveEmployeeInformation()
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener(('click'), function(event) {
+  addNewElementAsLi()
+  document.querySelector('form').reset()
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  document.querySelector("a").addEventListener(('click'), function(event) {
+    document.querySelector(".employee-list").innerHTML = ''
+  })
+}
