feb06e81538ee53028c3c37400f5b2aa90e5e168
Carol Greene <carolgreene3@gmail.com>
2018-11-10 19:30:23 +0000

    Done.

--- index.js
+++ index.js
@@ -7,28 +7,27 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-  return document.querySelector('input').value
+  return document.querySelector('input').value              //grab input element & return its value
 }
 
 function addNewElementAsLi() {
-  let element = document.createElement('li')
-  element.innerHTML = retrieveEmployeeInformation()
-  let ul = document.querySelector('ul')
-  ul.appendChild(element)
+  let li = document.createElement('li')                      //create new li element
+  li.innerHTML = retrieveEmployeeInformation()              //set value of li to equal return value of function above
+  document.querySelector('.employee-list').appendChild(li)   //select the employee-class ul & append the new li element to it
 }
 
 function addNewLiOnClick() {
-  const input = document.querySelector("input[type='submit']")
-  input.addEventListener('click', function(event) {
-    addNewElementAsLi()
-    document.querySelector('input').value = ""
+  let input = document.querySelector("input[type='submit']")  //grab input type submit & set variable to it
+  input.addEventListener('click', function(e) {               //add event listener to that variable & listen for click
+    addNewElementAsLi()                                        //when clicked, call addNewElementAsLi function
+    document.querySelector('input').value = ''                 //grab the input value & clear it
   })
 }
 
-  function clearEmployeeListOnLinkClick() {
-    let link = document.querySelector('a')
-    let list = document.querySelector('.employee-list')
-    link.addEventListener('click', function(event) {
-    list.innerHTML = ''
-    })
-  }
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')                  //grab a tag element & set variable to it
+  let list = document.querySelector('ul')                 //grab ul element & set variable to it
+  link.addEventListener('click', function(e) {            //add event listener to a tag variable & listen for click
+  list.innerHTML = ''                                     //clear innerHTML of the ul variable
+  })
+}
