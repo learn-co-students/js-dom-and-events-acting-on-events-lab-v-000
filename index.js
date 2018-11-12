4c6b4b7231993364ffe8f9aa04038512d90141f5
pracniscate <lorena.giddens@outlook.com>
2018-11-11 19:32:27 -0500

    finish lesson

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
-}
+  document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
+};
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+};
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`);
+};
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function (e) {
+    addNewElementAsLi();
+    input.value = '';
+  });
+};
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function (e) {
+    ul.innerHTML = '';
+  });
+};
