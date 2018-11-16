1b76a71876232022277751086a0a84148eb31335
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-16 17:58:30 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return $("input[name=name]").val().toString()
+}
+
+function addNewElementAsLi() {
+  $('ul.employee-list').append(`<li>${retrieveEmployeeInformation()}</li>`)
+}
+
+function addNewLiOnClick() {
+  $('input[type=sumbit').on('click', addNewElementAsLi())
+  $('input[name=name').val('')
+}
+
+function clearEmployeeListOnLinkClick() {
+  $('a').text('Clear Employee List').on('click', function() {
+    $('ul.employee-list').empty()
+  })
+}
