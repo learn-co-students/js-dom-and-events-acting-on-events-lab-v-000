6e37594f508322daf8eb79f42f5c4fe401c89eac
bkhartmire <bkhartmire@gmail.com>
2018-11-17 23:22:07 -0600

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let newLi = document.createElement('li')
+  newLi.innerHTML = retrieveEmployeeInformation()
+  document.querySelector("ul.employee-list").appendChild(newLi)
+}
+
+function addNewLiOnClick() {
+  let button = document.querySelector('input:last-child')
+  button.addEventListener('click', addNewElementAsLi())
+  document.querySelector('input').value = ''
+}
+
+function clearEmployeeListOnLinkClick() {
+
+}
