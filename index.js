f84e81b56af1845052ef1f764482c589632037fd
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-18 03:49:30 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+const input = document.querySelector('input')
+function retrieveEmployeeInformation(){
+  return document.getElementsByName("name")[0].value
+}
+
+
+
+function addNewElementAsLi(){
+  let name = retrieveEmployeeInformation()
+  return document.querySelector('.employee-list').append(`<li>${name}</li>`)
+}
+
+function addNewLiOnClick(){
+  let name = retrieveEmployeeInformation()
+  let complete = document.querySelector('.employee-list').append(`<li>${name}</li>`)
+  input.value = ""
+  return complete
+}
+
+function clearEmployeeListOnLinkClick(){
+  let link = document.querySelector('a')
+  let list = document.querySelector('ul')
+  link.addEventListener('click', function(e){
+    list.innerHTML = ""
+  })
+}
