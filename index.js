27e446921588b871880faa79d025dfc22337ef1d
monksandninjas <blackbeltdiaries@gmail.com>
2018-11-23 12:02:33 -0800

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+
+  li.innerHTML = retrieveEmployeeInformation();
+
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelectorAll('input')[1];
+  let clear  = document.querySelectorAll('input')[0];
+
+  submit.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    clear.value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clear = document.querySelector('a');
+  let ul    = document.querySelector('ul.employee-list');
+
+  clear.addEventListener('click', function(e) {
+    ul.innerHTML = '';
+  });
+}
