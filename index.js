acf21dfc9cdf913e8d616565cbd801d158826980
Kkelly22 <kk504408@gmail.com>
2018-11-26 15:24:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
@@ -5,3 +6,30 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+
+function retrieveEmployeeInformation () {
+	return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+	document.querySelector(".employee-list").append(retrieveEmployeeInformation());
+}
+
+function addNewLiOnClick() {
+	document.querySelector('input').addEventListener('click', addNewElementAsLi());
+	document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick () {
+	document.querySelector("a").addEventListener('click', clearList);
+}
+
+function clearList() {
+	document.querySelector(".employee-list").innerHTML = "";
+}
+
+
+
+
