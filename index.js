4cb1a86a955e5bbb35d0122803fa6ea6f13f9f00
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-25 19:22:00 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+ return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  // retrieve the retrieveEmployeeInformation
+  // grab employee List
+  //append data
+  var list =  document.querySelector(`.employee-list`);
+  var data = retrieveEmployeeInformation();
+  return list.insertAdjacentHTML(`beforeend`,`<li>${data}</li>`);
+}
+
+function addNewLiOnClick() {
+  var data = addNewElementAsLi();
+  var input = document.querySelector('input');
+  input.value = ""
+  return data;
+}
+
+ function clearEmployeeListOnLinkClick() {
+  var list =  document.querySelector(`.employee-list`);
+  return while (list.hasChildNodes()) {
+  return list.removeChild(list.firstChild);
+}}
