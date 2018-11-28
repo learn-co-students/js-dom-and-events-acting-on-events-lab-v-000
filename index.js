9ae761eecaba4787c7d1be1fe070184d075bd4e8
Anu Khambete <anukhambete@gmail.com>
2018-11-28 20:39:52 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,30 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').appendChild(li);
+  document.querySelector('input').value = '';
+}
+
+function addNewLiOnClick () {
+  let button = document.querySelector("[value='Add Employee']");
+  button.addEventListener('click', addNewElementAsLi);
+
+}
+
+function clearEmployeeListOnLinkClick () {
+  let link = document.querySelector("a[href='#']");
+  link.addEventListener('click', function(e) {
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+  }
+      
+}
