b3604b2fa331cbd964a2546e72fc0bd48a19e1f5
Anu Khambete <anukhambete@gmail.com>
2018-11-28 20:58:34 +0000

    Done.

--- index.js
+++ index.js
@@ -20,15 +20,16 @@ function addNewElementAsLi() {
 function addNewLiOnClick () {
   let button = document.querySelector("[value='Add Employee']");
   button.addEventListener('click', addNewElementAsLi);
-
 }
 
 function clearEmployeeListOnLinkClick () {
   let link = document.querySelector("a[href='#']");
+  let ul = document.querySelector('.employee-list');
+  let children = ul.children;
   link.addEventListener('click', function(e) {
     while (ul.firstChild) {
       ul.removeChild(ul.firstChild);
     }
-  }
-      
+  });
+
 }
