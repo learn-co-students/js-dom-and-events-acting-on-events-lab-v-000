6ce4100e1974af80590916cc3796b2f20fdb744a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-12-02 00:08:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+      event.preventDefault();
     })
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let name = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeEnd', `<li>${name}</li>`);
+}
+
+function addNewLiOnClick() {
+  let click = document.querySelector('input[type="submit"]');
+  click.addEventListener('click', function(event){
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let ul = document.querySelector('ul');
+  let click = document.querySelector('a');
+  click.addEventListener('click', function(event) { ul.innerHTML = ''; });
+}
