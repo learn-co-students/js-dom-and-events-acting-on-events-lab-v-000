26d2dd1308a099ac5e9b85f357de9fefa6fb7fdf
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-30 20:14:47 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let element = document.createElement('li');
+  element.innerHTML = retrieveEmployeeInformation();
+  let list = document.querySelector('.employee-list');
+  list.append(element);
+}
+
+function addNewLiOnClick() {
+   let submit = document.querySelector("input[type= 'submit']");
+   submit.addEventListener('click', function(e) {
+     addNewElementAsLi();
+     input.value = '';
+   });
+ }
+ 
+ function clearEmployeeListOnLinkClick() {
+   let link = document.querySelector('a'); 
+   let ul = document.querySelector('ul');
+   link.addEventListener('click', function(e) {
+     ul.innerHTML = "";
+   });
+ }
