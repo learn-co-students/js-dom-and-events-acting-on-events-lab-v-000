0a60eb122a605f32597952d94d42f0c139893b7d
SMartinSci <sm674@cornell.edu>
2019-01-01 13:52:53 -0800

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let Name = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${Name}</li>`)
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function (event) {
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let first = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  first.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput() {
+  document.querySelector('input').value = ''
+}
