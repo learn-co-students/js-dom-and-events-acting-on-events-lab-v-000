26b02bdcb7346e88db743e4e75372f36f421da54
rcrdlbl <bluh@outlook.com>
2019-01-02 14:52:58 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const main = $('main')
+function retrieveEmployeeInformation() {
+  const input = $('input')
+  return input.val()
+}
+
+function addNewElementAsLi() {
+  const list = $('.employee-list')
+  const newLi = document.createElement('li')
+  const newContent = document.createTextNode(`${retrieveEmployeeInformation()}`)
+  newLi.appendChild(newContent)
+  list.append(newLi)
+}
+// body > main > form > input[type="submit"]:nth-child(3)
+function addNewLiOnClick() {
+  const button = $('body main form input[type="submit"]')
+  button[0].addEventListener('click', function(e) {
+    addNewElementAsLi()
+    $('input').val("")
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  const list = $('.employee-list')
+  const clearLink = $('a')
+  clearLink[0].addEventListener('click', function(e) {
+    list.empty()
+  })
+}
