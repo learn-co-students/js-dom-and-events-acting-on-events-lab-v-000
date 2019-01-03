badbfe3bc0640fc52e0ac96c3c565f5377ea0347
bethurban <beth.urban@gmail.com>
2019-01-02 22:49:06 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+document.addEventListener('submit', retrieveEmployeeInformation);
+
+document.addEventListener('click', clearEmployeeListOnLinkClick);
+
+function retrieveEmployeeInformation() {
+  return document.getElementsByName("name")[0].value;
+}
+
+function addNewElementAsLi() {
+  let name = retrieveEmployeeInformation();
+  $('ul').append('<li>' + name + '</li>');
+}
+
+function addNewLiOnClick() {
+  retrieveEmployeeInformation();
+  addNewElementAsLi();
+  $('form').each(function() {
+    this.reset();
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  $("ul").empty();
+}
