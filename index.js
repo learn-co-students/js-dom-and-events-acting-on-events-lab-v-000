cf8d1198f70d02f98fa46400975d317d35bcca80
Kachel Wittig <kjwittig@gmail.com>
2019-01-04 11:30:57 -0600

    Donezo

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const ul = document.querySelector('.employee-list')
+const input = document.querySelector('input[type=text]')
+const button = document.querySelector('input[type=submit]')
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
@@ -5,3 +9,26 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const li = document.createElement('li');
+  li.textContent = retrieveEmployeeInformation().toString()
+  ul.appendChild(li)
+  input.value = ""
+}
+
+function addNewLiOnClick() {
+  button.addEventListener('click', addNewElementAsLi)
+}
+
+function clearEmployeeListOnLinkClick() {
+  const link = document.querySelector('a')
+  link.addEventListener('click', function() {
+    const ul = document.querySelector('ul.employee-list')
+    ul.innerHTML = ""
+  })
+}
