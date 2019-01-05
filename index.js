3639df5efe4f9ef103747d7838b3e2430c4fef8d
BrittanyTinnin <bntinnin7@gmail.com>
2019-01-05 12:05:56 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let ul = document.querySelector('.employee-list')
+  let li = document.createElement('li')
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.appendChild(li)
+}
+
+function addNewLiOnClick() {
+  let press = document.querySelector('input[type="submit"]');
+  press.addEventListener('click', function(e) {
+   addNewElementAsLi();
+   document.querySelector('input').value = ''
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(e) {
+  ul.innerHTML = ""
+  })
+}
+
+
+
+
+// clearEmployeeListOnLinkClick(): This empties out the employee list when "Clear employee list" button clicked.
