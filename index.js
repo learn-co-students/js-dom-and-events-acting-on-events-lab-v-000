558230a04df28d1845aeac393b7a15faf6be1a51
github username <github email address>
2019-01-05 23:24:21 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+// This retrieves the employee information from the input.
+// It returns a string equal to the value in the input.
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener(click', function(event) {
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
