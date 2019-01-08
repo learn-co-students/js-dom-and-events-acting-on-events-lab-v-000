f4f6200cdb125dd179327321f2e46caa2056dd19
Madeline-Stark <madeline.stark1@gmail.com>
2019-01-07 19:18:19 -0800

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(input) {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let employee = retrieveEmployeeInformation();
+  let employeeList = document.querySelector('.employee-list');
+  let li = document.createElement('li');
+  li.innerHTML = employee;
+  employeeList.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  const input = document.querySelector('input[type="submit"]');
+  input.addEventListener('click', function(e) {
+    addNewElementAsLi(e);
+    document.querySelector('input[type="text"]').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  const employeeListButton = document.querySelector('a');
+  const employeeList = document.querySelector('.employee-list')
+  employeeListButton.addEventListener('click', function(event) {
+    employeeList.innerHTML = '';
+  });
+}
