73f7c7ab100daa60a7815bb224177b7567063441
github username <github email address>
2019-01-11 14:54:20 -0500

    complete

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let element = retrieveEmployeeInformation()
+
+  document.querySelector('.employee-list').append(element)
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener('click', addNewElementAsLi())
+
+  input.value = ''
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event) {
+    document.querySelector('ul').innerHTML = ''
+  })
+}
