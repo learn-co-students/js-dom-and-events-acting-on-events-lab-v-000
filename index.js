cfd65b139c352ac26b5e8aee4c34ec9c25ab476b
Mark Lawrence <lawrence.ml@gmail.com>
2019-01-12 20:28:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+function preventRefreshOnSubmit() {
+  document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+  })
+}
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', addNewElementAsLi());
+  document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clear = document.querySelector('a');
+  clear.addEventListener('click', function (e) {
+    document.querySelector('.employee-list').innerHTML = ""
+  })
 }
 
 preventRefreshOnSubmit()
