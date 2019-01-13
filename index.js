782351c3f270b27419b459615be9f36a43f3c21c
charlie  lee <ctlee9096@gmail.com>
2019-01-13 03:49:14 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector("input[type=text]").value
+  // debugger;
+}
+//https://stackoverflow.com/questions/4010356/form-validation-get-all-input-type-text-and-validate
+//https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascripts
+
+function addNewElementAsLi() {
+  const employee = retrieveEmployeeInformation()
+  // debugger;
+  console.log(employee)
+  const employeeList = document.querySelector('ul.employee-list')
+  const listItem = document.createElement('li');
+  employeeList.appendChild(listItem).innerHTML = employee;
+}
+//https://www.digitalocean.com/community/tutorials/how-to-make-changes-to-the-dom
+//how to create a list item in javascript
+//https://stackoverflow.com/questions/17773938/add-a-list-item-through-javascript
+//http://jsfiddle.net/Gmyag/
+
+function addNewLiOnClick(){
+  const submitButton = document.querySelector('input[type="submit"]')
+  submitButton.addEventListener('click', function(event){
+    addNewElementAsLi();
+    // debugger;
+    document.querySelector('input[type="text"]').value = ''
+    // debugger;
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  const employeeList = document.querySelector('ul.employee-list')
+  const clearEmployeeLink = document.querySelector('a')
+  clearEmployeeLink.addEventListener('click', function(event){
+    employeeList.innerHTML = ''
+  })
+}
+//https://www.digitalocean.com/community/tutorials/how-to-make-changes-to-the-dom
