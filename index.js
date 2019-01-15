19a2298b9cdfec6d01bab47fd6396e7677791d91
Ayana Bando <ayanaflax@yahoo.com>
2019-01-15 15:45:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+      event.preventDefault();
+    });
 }
 
-preventRefreshOnSubmit()
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi(){
+   document.querySelector('.employee-list').innerHTML = retrieveEmployeeInformation();
+}
+
+function addNewLiOnClick(){
+  document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
+  addNewElementAsLi();
+  document.querySelector('input').value = ''});
+  }
+
+function clearEmployeeListOnLinkClick(){
+  let ul = document.querySelector('ul');
+  document.querySelector('a').addEventListener('click', function(event){
+    ul.innerHTML = '';
+  });
+}
+
+
+
+
+preventRefreshOnSubmit();
