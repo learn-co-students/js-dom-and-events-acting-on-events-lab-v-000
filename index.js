ac744a9e83b7bc5bf3b6264cfc4b6d65a1b14788
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-01-22 19:56:14 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
+
+function clearEmployeeListOnLinkClick(){
+  let clear = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  clear.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
