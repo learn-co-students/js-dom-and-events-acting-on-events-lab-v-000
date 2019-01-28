acf8aac40461033c8d98da15a49cf1d237203c1f
epic0448 <samuelogundiran@Yahoo.com>
2019-01-28 11:26:18 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi(){
+  t = retrieveEmployeeInformation()
+  document.querySelector(".employee-list").append(t)
+}
+
+function addNewLiOnClick(){
+  v = document.querySelector("input[type='submit']")
+   v.addEventListener('click', function() {
+        addNewElementAsLi()
+
+      document.querySelector('input').value = ""
+
+  })
+}
+
+  function clearEmployeeListOnLinkClick(){
+  
+  p = document.querySelector("a")
+    p.addEventListener('click',function(){
+
+    document.querySelector('.employee-list').innerText = ""
+
+       })
+      }
