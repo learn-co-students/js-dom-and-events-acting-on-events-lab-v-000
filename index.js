3abe18c2adb1d847f46370d23d48736a0ea4bd61
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-04 12:58:49 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi(){
+  const element = document.createElement('li');
+  element.innerText = retrieveEmployeeInformation();
+  // const value = document.createTextNode(retrieveEmployeeInformation());
+  return document.querySelector('.employee-list').appendChild(element);
+}
+
+function addNewLiOnClick(){
+  return document.querySelector('input[type="submit"]').addEventListener("click", function() {
+    addNewElementAsLi();
+    document.querySelector('input[type="text"]').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+  return document.querySelector('a').addEventListener("click", function() {
+    document.querySelector('.employee-list').innerHTML = '';
+  });
+}
