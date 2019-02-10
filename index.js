745c926d7ba1e4445383b26ecf9e55d4d781f17a
JuanGongora <gongora.animations@gmail.com>
2019-02-10 17:25:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+  document.querySelector('form').addEventListener('submit', function(e){
+    event.preventDefault();
+  });
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation() {
+  let value = document.querySelector('input').value;
+  return value;
+}
+
+function addNewElementAsLi() {
+  let value = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${value}</li>`);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('[type=submit]');
+  submit.addEventListener('click', function(e){
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let list = document.querySelector('.employee-list');
+  let anchor = document.querySelector('a');
+  anchor.addEventListener('click', function(e){
+    list.innerHTML = '';
+  });
+}
