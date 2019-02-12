4bc41937bd959fbbccde918c8a685a5c7a694af1
kylek <27875726+kylekinnear@users.noreply.github.com>
2019-02-12 10:56:43 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+  return $('input')[0].value
+}
+
+function addNewElementAsLi() {
+  let newEmployee = `<li>${retrieveEmployeeInformation()}</li>`
+
+  $('.employee-list').append(newEmployee)
+}
+
+function addNewLiOnClick() {
+  $('input[type="submit"]')[0].addEventListener('click', function(e){
+    addNewElementAsLi()
+    $('input')[0].value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let list = $('ul.employee-list')[0]
+
+  $('a')[0].addEventListener('click', function(e){
+    while (list.firstChild) {
+      list.removeChild(list.firstChild)
+    }
+  })
+}
