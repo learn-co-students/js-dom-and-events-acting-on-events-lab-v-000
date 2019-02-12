c564f4601967ee9bfd8e19661207eba6b1761bad
freeWillee <will.ik.lee@gmail.com>
2019-02-12 12:38:01 -0700

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,31 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation(){
-  // Add listener to the main page to listen for keyboard input?
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let listElement =  document.createElement('li');
+
+  listElement.innerHTML = retrieveEmployeeInformation();
+
+  document.querySelector('.employee-list').appendChild(listElement);
+}
+
+function addNewLiOnClick() {
+
+  document.querySelector('form').addEventListener('submit', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function() {
+    let list = document.querySelectorAll('.employee-list li')
+
+    for (let i=0; i < list.length; i++) {
+      list[i].remove();
+    }
+  })
 }
