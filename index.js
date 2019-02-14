8f8cc576a6dc6b2b81a9861b53c28ff7da420931
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 22:16:19 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+  let field = document.querySelector('input');
+  return field.value;
+}
+
+function addNewElementAsLi() {
+  let originalList = document.querySelector('ul');
+  return originalList.innerHTML += retrieveEmployeeInformation();
+}
+
+function addNewLiOnClick() {
+  const button = document.querySelector("input[type='submit']");
+  button.addEventListener('click', function(e){
+    addNewElementAsLi();
+    resetInput();
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
