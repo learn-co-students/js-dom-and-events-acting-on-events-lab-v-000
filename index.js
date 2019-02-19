6fc8fa73f7c82a81b20052cdce8727f987937d1a
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-19 03:41:17 +0000

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 const input = document.querySelector("input");
+const employeeList = document.querySelector(".employee-list");
+const addEmployee = document.querySelector('input[type="submit"]');
+const clearEmployeeList = document.querySelector("a");
 
 function retrieveEmployeeInformation() {
   return input.value;
@@ -16,5 +19,20 @@ function addNewElementAsLi() {
   let element = document.createElement("li");
   let name = retrieveEmployeeInformation();
   element.innerHTML = name;
-  return document.querySelector(".employee-list").appendChild(element);
+  return employeeList.appendChild(element);
+}
+
+function addNewLiOnClick() {
+  addEmployee.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    input.value = "";
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  clearEmployeeList.addEventListener('click', function(event) {
+    while (employeeList.firstChild) {
+      employeeList.removeChild(employeeList.firstChild);
+    }
+  });
 }
