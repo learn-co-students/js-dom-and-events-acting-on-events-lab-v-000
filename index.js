7a868c062eefe0618a5667c4c31f6beb27faf3fe
Keith <fkmccallion@gmail.com>
2019-02-25 05:33:11 -0800

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let textInput = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return textInput.value;
+}
+
+function addNewElementAsLi() {
+  let element = document.createElement('li');
+  element.innerHTML = retrieveEmployeeInformation();
+
+  let element2 = document.querySelector('ul.employee-list');
+  element2.appendChild(element);
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
+    addNewElementAsLi()
+    textInput.value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(event) {
+    let ul = document.querySelector('ul.employee-list');
+    ul.innerHTML = '';
+  })
+}
