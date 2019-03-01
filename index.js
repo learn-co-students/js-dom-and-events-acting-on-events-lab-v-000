d0ff4f3cdcb208b2ac1effd8f86e7d58032db949
github username <github email address>
2019-03-01 12:27:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
+
     })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const li = document.createElement('li')
+  const liText = document.createTextNode(retrieveEmployeeInformation());
+  li.appendChild(liText);
+  document.querySelector('.employee-list').appendChild(li)
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+
+  document.querySelector('a').addEventListener('click', function() {
+    const unorderedList = document.querySelector(".employee-list")
+
+    while (unorderedList.hasChildNodes()) {
+      unorderedList.removeChild(unorderedList.firstChild);
+    }
+  })
+}
