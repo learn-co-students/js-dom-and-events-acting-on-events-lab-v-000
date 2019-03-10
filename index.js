bd2f295aede4a8343fb7d00bfb2cb4f3f4014352
amandena <manda.panda91@yahoo.com>
2019-03-09 19:47:01 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let element = document.createElement('li');
+  let li = document.querySelector('.employee-list').appendChild(element);
+  li.innerHTML = retrieveEmployeeInformation();
+}
+
+function addNewLiOnClick() {
+  let form = document.querySelector('form');
+  form.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = '';
+  });
+}
