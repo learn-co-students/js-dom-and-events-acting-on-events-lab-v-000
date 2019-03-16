e1cd5177cde4418b52c1a4e243729aa4f16f99d4
Steve Albonico <salbonico@gmail.com>
2019-03-16 12:29:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
+
     })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi(){
+  newThing = retrieveEmployeeInformation()
+ $('.employee-list').append(`<li>${newThing}</li>`)
+}
+
+function addNewLiOnClick(){
+  let button = document.querySelector('input[type="submit"]')
+  button.addEventListener('click',function(){addNewElementAsLi()
+  $('input[type="text"]').val('')
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let button = document.querySelector('a')
+  button.addEventListener('click',function(){addNewElementAsLi()
+  $('.employee-list').empty()
+  })
+}
