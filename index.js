6d339fd342093c387e175e8a45bdae14f31bf35b
Richard-Frost <frost.rchrd@gmail.com>
2019-03-19 13:05:36 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let userInput = document.querySelector("form input")
+
+function retrieveEmployeeInformation(){
+	return userInput.value 
+}
+
+function addNewElementAsLi(){
+  let element = document.createElement('li');
+  element.innerHTML = retrieveEmployeeInformation()
+  document.querySelector("ul.employee-list").appendChild(element)
+}
+
+function addNewLiOnClick(){
+  let element = document.querySelector('input[type="submit"]')
+  element.addEventListener('click', function(event) {
+    addNewElementAsLi()
+	userInput.value = ''
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+
+  let list = document.querySelector('ul')
+  let a = document.querySelector('a')
+  a.addEventListener('click', function(event){
+    list.innerHTML = ''
+  });	
+}
+
+
