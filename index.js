762948f78b79c26706543107202b438ba8f07268
Brad Schrag <brad.schrag@gmail.com>
2019-03-21 22:27:35 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+	const name = document.getElementsByName('name')[0].value
+	return name;
+}
+
+function addNewElementAsLi() {
+	const ul = document.getElementsByClassName('employee-list')[0]
+	const li = document.createElement('li')
+	li.innerHTML = retrieveEmployeeInformation()
+	ul.append(li)
+}
+
+function addNewLiOnClick() {
+	const input = document.getElementsByName('name')[0]
+	addNewElementAsLi()
+	input.value = '';
+}
+
+function clearEmployeeListOnLinkClick() {
+	const ul = document.getElementsByClassName('employee-list')[0]
+	console.log(ul.childNodes)
+	const link = document.querySelector('a')
+
+	link.addEventListener('click', function() {
+		ul.innerHTML = ''
+	});
+}
