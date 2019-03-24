ba56caa61775f52520fcd0d56b8793a3e8db17cc
Elizabeth <efkarst@outlook.com>
2019-03-24 13:34:51 -0700

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,31 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
-
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+    return document.getElementsByName('name')[0].value
+}
+
+function addNewElementAsLi() {
+  let ul = document.getElementsByClassName('employee-list')[0];
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  document.querySelector('form').addEventListener('submit', addNewElementAsLi())
+  document.getElementsByName('name')[0].value = '';
+}
+
+function clearEmployeeList() {
+  let ul = document.getElementsByClassName('employee-list')[0];
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild)
+  }
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.getElementById('clear-list').addEventListener('click', clearEmployeeList)
+}
