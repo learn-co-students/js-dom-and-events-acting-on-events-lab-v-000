56518118cd9d773554992a0d2ff07242861fc7b1
Justine B <justinegayton@gmail.com>
2019-04-01 20:55:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+  })
+}
+preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(input){
+  let employee = document.querySelector('input').value
+  return employee;
 }
 
-preventRefreshOnSubmit()
+function addNewElementAsLi(){
+  $('ul.employee-list').append(retrieveEmployeeInformation());
+}
+
+function clearFormField() {
+  document.getElementById('myform').reset();
+}
+
+function addNewLiOnClick(event){
+  let submit = $('form input:last')[0];
+  submit.addEventListener('click', event => {
+    addNewElementAsLi();
+    clearFormField();
+    return false;
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let x = document.getElementById('clear')
+  x.addEventListener('click', event => { event.preventDefault()
+  let list = document.getElementById('elist')
+  list.innerHTML = '';
+  })
+}
