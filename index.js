74f9e23db21dbd68f42518bbe1da6f3ad32f13ff
alekvuka <alekvuka@gmail.com>
2019-04-05 19:38:24 -0400

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,32 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation() {
+  return 'Bob'
+}
+
+
+function addNewElementAsLi() {
+  let section = document.querySelector('ul.employee-list')
+  let element = document.createElement("li");
+  element.innerText = 'Bob'
+  section.append(element)
+}
+
+addNewLiOnClick() {
+  let ele = document.querySelector('input[type="submit"]')
+
+  ele.addEventListener('click', function(event) {
+    let section = document.querySelector('ul.employee-list')
+    let element = document.createElement("li");
+    element.innerText = 'Bob'
+    section.append(element)
+  } )
+
+
+}
+
+
+
+
 preventRefreshOnSubmit()
