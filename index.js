82f96a67e8a2bd8f868ef56728528b77fb0dd7d7
Christopher Beltran <christod.beltran@gmail.com>
2019-04-06 15:46:24 -0600

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+let employee = retrieveEmployeeInformation();
+$('.employee-list').append(employee);
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').onclick = function() {
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  }
+};
+
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').onclick = function() {
+    $('.employee-list').empty()
+  }
+}
