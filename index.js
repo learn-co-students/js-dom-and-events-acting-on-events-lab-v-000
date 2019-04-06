b63a53472b119d3769673f67987665d53db4fd8b
alekvuka <alekvuka@gmail.com>
2019-04-05 20:36:18 -0400

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function retrieveEmployeeInformation() {
   return 'Bob'
 }
 
-
 function addNewElementAsLi() {
   let section = document.querySelector('ul.employee-list')
   let element = document.createElement("li");
@@ -16,19 +15,35 @@ function addNewElementAsLi() {
   section.append(element)
 }
 
-addNewLiOnClick() {
+function addNewLiOnClick() {
   let ele = document.querySelector('input[type="submit"]')
 
   ele.addEventListener('click', function(event) {
-    let section = document.querySelector('ul.employee-list')
-    let element = document.createElement("li");
-    element.innerText = 'Bob'
-    section.append(element)
+   let section = document.querySelector('ul.employee-list')
+   let element = document.createElement("li");
+   element.innerText = document.querySelector('input').value
+   section.append(element)
+   document.querySelector('input').value = ""
   } )
-
 
 }
 
+function clearEmployeeListOnLinkClick() {
+
+
+
+      let ele = document.querySelector('a')
+      let target = document.querySelector('.employee-list')
+      ele.addEventListener('click', function(event) {
+          let elements = target.querySelectorAll('li')
+          elements[0].remove()
+          elements[1].remove()
+          target.remove()
+  // // //   document.querySelector('.employee-list')
+            target.innerText = ""
+      })
+
+}
 
 
 
