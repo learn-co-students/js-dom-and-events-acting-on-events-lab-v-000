7429c57d7e536f2c3cbad768dbe1ffc6761e853f
nateshuster <nshu521992@gmail.com>
2019-04-12 22:45:04 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const ul = document.querySelector('.employee-list')
+  let li = document.createElement('li')
+  li.appendChild(document.createTextNode(`${retrieveEmployeeInformation()}`));
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function () {
+    addNewElementAsLi()
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  link = document.querySelector('a')
+  link.addEventListener('click', function () {
+    const ul = document.querySelector('.employee-list')
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+  })
+}
