a2fe1bd040597ed29e0e886ca05197e063610e90
mattetress <mattetress@me.com>
2019-04-22 02:50:49 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let name = retrieveEmployeeInformation();
+  let list = document.querySelector('ul.employee-list');
+  let li = document.createElement("li");
+  li.innerHTML = name;
+  list.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener("click", function(event) {
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector("a")
+
+  link.addEventListener("click", function(event) {
+    document.querySelector('ul.employee-list').innerHTML = "";
+  })
+}
