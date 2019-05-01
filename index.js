729dd1883470868bb3a3f18ff9aac8140ed6f868
FionaDL <github email address>
2019-05-01 23:07:20 +0200

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input= document.querySelector("input")
+
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let value = retrieveEmployeeInformation()
+  document.querySelector(".employee-list").append(value)
+}
+
+function addNewLiOnClick() {
+  let thisValue = document.querySelector('input[type="submit"]')
+  thisValue.addEventListener('click', function(event) {
+    addNewElementAsLi()
+    input.value = ""
+    }
+  )
+}
+
+function clearEmployeeListOnLinkClick() {
+  let employeeButton = document.querySelector("a")
+  let employeeList = document.querySelector("ul")
+  employeeButton.addEventListener('click', function(event) {
+    employeeList.innerHTML = ""
+  }
+)
+}
