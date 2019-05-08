c3c5f23d4dfd9346ef0195a5905502ce523d20fd
MDAM182 <mdam2345@gmail.com>
2019-05-08 01:05:07 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value
+
+}
+
+function addNewElementAsLi(){
+  let ul = document.querySelector('ul')
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation()
+  return ul.appendChild(li);
+}
+
+function addNewLiOnClick(){
+    let submit = document.querySelector('input[type="submit"]')
+    return  submit.addEventListener('click', function(e) {
+        addNewElementAsLi()
+      document.querySelector('input').value = ''
+    })
+  }
+
+function clearEmployeeListOnLinkClick(){
+    let link = document.querySelector('a')
+    let ul = document.querySelector('ul')
+    link.addEventListener('click', function(event){
+      ul.innerHTML = ''
+    })
+  }
