172531427faea61f6f269f4a9ac21bb2e576a21a
RonSala <ron.sala@gmail.com>
2019-05-16 02:05:27 -0400

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-    return document.querySelector('input').value;
+  return document.querySelector('input').value;
 }
 
+function addNewElementAsLi() {
+  employee = retrieveEmployeeInformation();
+  let node = document.createElement('li');
+  let textnode = document.createTextNode(employee);
+  node.appendChild(textnode);
+  document.querySelector('ul').appendChild(node);
+}
+
+function addNewLiOnClick() {
+  let el = document.querySelector('input');
+  let text = el.value;
+
+  function newTextNode(text) {
+    document.querySelector('input').value = '';
+    let textnode = document.createTextNode(text);
+    return textnode;
+  }
+
+  let node = document.createElement('li');
+  el.addEventListener('click', newTextNode(text));
+  let textnode = newTextNode(text);
+  node.appendChild(textnode);
+  document.querySelector('ul').appendChild(node);
+}
+
+function clearEmployeeListOnLinkClick() {
+  let el = document.querySelector('a');
+
+  function clearEmployeeList() {
+    ul = document.querySelector('ul');
+    ul.innerHTML = '';
+  }
+  
+  el.addEventListener('click', clearEmployeeList);
+}
