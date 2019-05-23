0236042a499809b77af829d6bb304ad9c6c17e0f
Erin Green <erin.y.green@gmail.com>
2019-05-23 18:15:22 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  const inputs = document.getElementsByTagName("input")
+  return inputs[0].value
+
+  // inputs[1].addEventListener('click', function(event) {
+  //   return inputs[0].value
+  // })
+}
+
+function addNewElementAsLi() {
+  // const ul = document.getElementsByClassName('employee-list')
+  const li = document.createElement('li')
+  const text = document.createTextNode(retrieveEmployeeInformation())
+  li.innerHtml = retrieveEmployeeInformation();
+
+  document.getElementsByClassName('employee-list').appendChild(li)
+}
+
+function addNewLiOnClick() {
+
+}
+
+function clearEmployeeListOnLinkClick() {
+
+}
