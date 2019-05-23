e9e513e135598ad249faa92421b83ba562aa8015
Erin Green <erin.y.green@gmail.com>
2019-05-23 18:40:05 +0000

    Done.

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ preventRefreshOnSubmit()
 function retrieveEmployeeInformation() {
   const inputs = document.getElementsByTagName("input")
   return inputs[0].value
-
-  // inputs[1].addEventListener('click', function(event) {
-  //   return inputs[0].value
-  // })
 }
 
 function addNewElementAsLi() {
@@ -26,12 +22,20 @@ function addNewElementAsLi() {
 
 function addNewLiOnClick() {
   const submit = document.getElementsByTagName("input")[1]
+  const input = document.getElementsByTagName("input")[0]
 
-  submit.addEventListener('click', function(event) {
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi();
+    input.value = ""
+  })
 
-  }
 }
 
 function clearEmployeeListOnLinkClick() {
+  const ul = document.getElementsByClassName('employee-list')[0]
+  const clearButton = document.getElementsByTagName('a')[0]
 
+  clearButton.addEventListener('click', function(event){
+    ul.innerHTML = ""
+  })
 }
