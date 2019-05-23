da354109005d5774132ea5bb665ee1bf6e15d0ce
Katie Moosman <moosman.katie@gmail.com>
2019-05-22 23:26:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+  })
+}
+
+function addNewLiOnClick(){
+  document.querySelector('input[type="submit"]').addEventListener('click', addNewLi)
+}
+
+function clearEmployeeListOnLinkClick(){
+  document.querySelector('a').addEventListener('click', killList)
+}
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('form > input').value;
+}
+
+function addNewElementAsLi (){
+let node = document.createElement('li');    
+let textnode = document.createTextNode(retrieveEmployeeInformation());
+
+node.appendChild(textnode);
+
+let list = document.getElementsByClassName('employee-list')[0];
+list.appendChild(node);
+
+return true;
+}
+
+function addNewLi(event) {
+  addNewElementAsLi();
+  document.querySelector('form > input').value = '';
+  return true;
+}
+
+function killList() {
+  const myList = document.getElementsByClassName('employee-list')[0];
+  myList.innerHTML = '';
+  return true;
 }
 
 preventRefreshOnSubmit()
