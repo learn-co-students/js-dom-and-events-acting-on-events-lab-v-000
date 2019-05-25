fafe54da36f4a6f589928911bff252ede5339a83
Josef Benninghoff <jsbenning4@gmail.com>
2019-05-25 19:11:51 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,34 @@ function preventRefreshOnSubmit(){
     })
 }
 
-preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  let myInput = document.querySelector('input').value;
+  return myInput;
+}
+
+function addNewElementAsLi(){
+  let myList = document.querySelector('.employee-list');
+  let li = document.createElement("li");
+
+  li.appendChild(document.createTextNode(retrieveEmployeeInformation()));
+  myList.appendChild(li);
+}
+
+function addNewLiOnClick(){
+  let adder = document.querySelectorAll("input[value='Add Employee']");
+  adder.onclick = addNewElementAsLi();
+  document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick(){
+  let links = document.getElementsByTagName('a');
+  let myList = document.querySelector('.employee-list');
+  links[0].onclick = function(){
+    myList.innerHTML = "";
+  };
+}
+
+preventRefreshOnSubmit();
+
+// http://67.205.182.198:48550
