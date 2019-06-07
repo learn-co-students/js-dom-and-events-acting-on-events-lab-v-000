5a3efe344044cfedae1f6be38097d9767fe9f8e3
ryanmanchester <ryanm17@gmail.com>
2019-06-06 21:01:18 -0400

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,32 @@ function preventRefreshOnSubmit(){
     })
 }
 
+
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi(){
+  let employeeInfo = retrieveEmployeeInformation();
+  let ul = document.querySelector('.employee-list');
+  let li = document.createElement('li');
+  ul.append(li);
+  return li.append(employeeInfo);
+}
+
+function addNewLiOnClick() {
+  let submitButton = document.querySelector('input[type= submit]')
+  submitButton.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clearLink = document.body.querySelector('a');
+  clearLink.addEventListener('click', function(e) {
+    document.querySelector('ul').innerHTML = "";
+});
+}
