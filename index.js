fa145932cc8cddd44092fd821f495ac9f96759f7
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-07-05 12:27:03 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input[type="text"]').value
+};
+
+function addNewElementAsLi() {
+  let ul = document.getElementsByClassName("employee-list")[0];
+  let newEle = document.createElement("li");
+  newEle.innerHTML =  retrieveEmployeeInformation().toString();
+  ul.appendChild(newEle);
+};
+
+function addNewLiOnClick() {
+
+  document.querySelector('input[type="submit"]').addEventListener('click', function(event) {
+    event.preventDefault()
+    addNewElementAsLi();
+    document.querySelector('input[type="text"]').value = '';
+    }  )
+
+};
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function() {
+    let list = document.getElementsByClassName("employee-list")[0];
+    list.innerHTML = ''
+  })
+};
