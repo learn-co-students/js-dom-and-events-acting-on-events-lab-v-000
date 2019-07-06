d556767a339fb826d6e4e073b897b963b82338f3
dhintz <dhintz89@gmail.com>
2019-07-05 22:49:00 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input[name="name"]').value;
+}
+
+function addNewElementAsLi() {
+  const employeeList = document.querySelector('ul.employee-list');
+  let newEmployee = document.createElement('li');
+  newEmployee.innerHTML = retrieveEmployeeInformation();
+  employeeList.appendChild(newEmployee);
+  document.querySelector('input[name="name"]').value = "";
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener(
+    'click', addNewElementAsLi
+  );
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', clearList);
+
+  function clearList() {
+    const emList = document.querySelector('ul.employee-list')
+    while (emList.childElementCount > 0) {
+      emList.removeChild(emList.childNodes[0]);
+    }
+  }
+}
