55d0fae00ae17aa11a04650cadf9b114f38b875d
christianavdm <christianavdm@gmail.com>
2019-07-25 17:44:35 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,46 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('#employee-name').value; 
+}
+
+function addNewElementAsLi() {
+  let employeeList = document.querySelector("#employee-list"); 
+  let element = document.createElement('li'); 
+  element.innerHTML = retrieveEmployeeInformation(); 
+
+  employeeList.appendChild(element); 
+
+  document.querySelector('#employee-name').value = ""; 
+
+}
+
+function addNewLiOnClick() {
+  const button = document.querySelector('#button');
+
+  button.addEventListener('click', addNewElementAsLi);
+}
+
+function clearEmployeeListOnLinkClick() {
+  const button = document.querySelector('#clear');
+
+/*   function clearList() {
+
+    let employeeList = document.querySelector("#employee-list"); 
+    let child = employeeList.lastElementChild; 
+ 
+     while (child) { 
+        employeeList.removeChild(child); 
+        child = employeeList.lastElementChild; 
+    }  
+  }
+  button.addEventListener('click', clearList()); */
+
+  button.addEventListener('click', function() {
+    let employeeList = document.querySelector("#employee-list"); 
+    employeeList.innerHTML = ""; 
+  });
+
+}
