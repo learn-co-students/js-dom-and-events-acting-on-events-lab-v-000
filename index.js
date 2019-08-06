ed4acce374a217a5966f0d10cba90881c3485771
Heshie Brody <4hbrody@gmail.com>
2019-08-06 19:17:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function preventRefreshOnSubmit(){
+function preventRefreshOnSubmit () {
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+      event.preventDefault();
     })
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation () {
+  return $('input:text').val();
+}
+
+function addNewElementAsLi () {
+  let ul = $('.employee-list');
+  let name = retrieveEmployeeInformation();
+
+  ul.append(`<li>${name}</li>`);
+}
+
+function addNewLiOnClick () {
+  addNewElementAsLi ();
+  $('input:text').val('');
+}
+
+function clearEmployeeListOnLinkClick () {
+  let link = document.querySelector('a');
+  link.addEventListener('click', function () {
+    $('.employee-list').empty();
+  });
+}
