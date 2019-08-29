e7b7883499494b4dbf7551c1a3b56ed9041e3e00
github username <github email address>
2019-08-28 20:34:39 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  const einfo = document.querySelector('input').value;
+  return einfo;
+}
+
+function addNewElementAsLi() {
+  const ul = $('ul.employee-list');
+  const li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.append(li);
+}
+
+function addNewLiOnClick() {
+  const submit = $('input')[1];
+  submit.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  const list = $('ul.employee-list')[0];
+  const link = $('a')[0];
+  link.addEventListener('click', function(e) {
+    e.preventDefault;
+    list.innerHTML = '';
+  })
+}
