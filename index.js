2ae2e34b141c09f637da4928f41682f58c1907cb
Allison Howard <alliemhow@gmail.com>
2019-09-05 06:28:36 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let n = retrieveEmployeeInformation();
+  return document.querySelector(".employee-list").append(n);
+}
+
+function addNewLiOnClick() {
+  document
+    .querySelector('input[type="submit"]')
+    .addEventListener('click', function(event) {
+      addNewElementAsLi();
+      resetInput();
+    });
+}
+
+function resetInput() {
+  document.querySelector("input").value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = ''
+  });
+}
